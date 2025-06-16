7652abcbd489710dd0867bf666e4e664a05891f5
codeflow25 <mussg2540@gmail.com>
2025-06-16 20:38:24 +0900

    자료형 주석

--- JS/js/2_variable.js
+++ JS/js/2_variable.js
@@ -48,3 +48,41 @@
         document.write("매개변수 num1="+num1+", num2 ="+num2);
     }
 */
+
+// ? 만들기 (ex> 선언(function) 이름(name)(){}) - 선언(var) 이름(name); -> 변수를 선언하였다. 
+// ! var(용도정의없는 기본변수) | let(상시 - 언제든 값을 변환하고 변경하며 옮길 수 있는) / const(고정 - 함부로 값을 변환하거나 변경해서는 안되는)
+
+/* 
+    * 전역변수 : 여러명이서 햄버거를 먹을 때 감자튀김을 다같이 붓고 먹는다면?
+    ? 햄버거는 내 개인의 소유, 다른 사람이 먹지 않음
+    ? 감자튀김은 모두가 같이 먹으려고 부은거니 모두가 자유롭게 먹을 수 있음
+    ! 햄버거 : 지역변수 / 감자튀김 : 전역변수
+*/
+
+// case.1 변수(전역) 만들기
+var userName; // 변수 선언
+
+// case.2 변수 선언 및 초기화
+userName = 'Jeong'; // 위에서 선언된 전역변수 초기화
+var userAge = 30; // 선언 및 초기화
+
+// case.3 다중 선언 및 초기화
+var radius, pi;
+radius = 10, pi= 3.141592;
+
+// case.4 변수의 특성
+var cup = 'Coffee';
+// console.log(cup);
+cup = 'Tea';
+// console.log(cup);
+cup = 'juice';
+console.log(cup);
+// alert(cup);
+// document.write(cup);
+
+// todo. 자신의 나이를 cliAge 변수에 담고 출력한 다음 다음 출력에서는 20년 뒤 나이를 출력
+
+var cliAge = 30;
+console.log("저의 현재 나이는 "+cliAge+"세 이고,");
+cliAge = cliAge + 20;
+console.log("20년 뒤에는 "+cliAge+"세 입니다.");
