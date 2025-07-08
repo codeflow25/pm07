ffad78d21342a478ac700c26e0ed2929f7d4b2c4
codeflow25 <mussg2540@gmail.com>
2025-07-08 21:40:20 +0900

    todo 힌트

--- JQUERY/js/5_foldTODO.js
+++ JQUERY/js/5_foldTODO.js
@@ -1,5 +1,11 @@
-// ().click(function(){?(열,width,height)}) * 10
+$(function(){
+    gallery(0,200,200);
+    $(".1way").click(function(){gallery(1,1000,1000)});
+});
+function gallery(count, imgWidth, imgHeight){
+    var $images = $("#imgContainer img");
+    var $imgLength = $images.length;
+    var xpos, ypos;
 
-// function ?(1,2,3){
 
-// }
+}
